5fd73e99808a724081168540be804a13f89e1a7c
User Name <User Name@LUPITA>
2024-02-09 09:35:11 -0500

    Agregar archivos de proyecto.

---
+++ calcular_sueldo_de_un_trabajador/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("sueldo_de_los_trabajadores");
+Console.WriteLine();
+
+
+Console.WriteLine("Ingresa el pago por horas: ");
+double pagoporHoras = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingresar las horas trabajadas: ");
+double HorasTrabajadas = double.Parse(Console.ReadLine());
+
+
+double sueldoBruto = pagoporHoras * HorasTrabajadas;
+double AFP = sueldoBruto * 0.0287;
+double SFS = sueldoBruto * 0.0304;
+double TotalDescuento = AFP + SFS;
+double sueldoNeto = sueldoBruto - TotalDescuento;
+
+Console.WriteLine("Sueldo bruto: " + sueldoBruto);
+Console.WriteLine("AFP: " + AFP);
+Console.WriteLine("SFS: " + SFS);
+Console.WriteLine("Sueldo neto: " + sueldoNeto);
+
+
+
+
+
+
+
